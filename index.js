2228b0833f8d403443ea23b0ed5477ed10505d54
salvatoluice <salvatoluice5@gmail.com>
2023-05-13 23:41:14 +0300

    Solution

--- index.js
+++ index.js
@@ -226,3 +226,28 @@ return dp[n];
 // QUIZ 9
 
 // Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+  let currentNode = head;
+
+  while (currentNode !== null && currentNode.next !== null) {
+    if (currentNode.val === currentNode.next.val) {
+      currentNode.next = currentNode.next.next;
+    } else {
+      currentNode = currentNode.next;
+    }
+  }
+
+  return head;
+};
