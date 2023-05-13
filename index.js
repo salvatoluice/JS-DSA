62278e3ca9f9e1e390fb5abb3450e5c1583992b9
salvatoluice <salvatoluice5@gmail.com>
2023-05-13 23:36:59 +0300

    question 8

--- index.js
+++ index.js
@@ -201,3 +201,24 @@ return Math.floor(result);
 // You are climbing a staircase. It takes n steps to reach the top.
 
 // Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+  if (n === 1) {
+  return 1;
+}
+
+let dp = [];
+dp[1] = 1; // Number of ways to climb 1 step
+dp[2] = 2; // Number of ways to climb 2 steps
+
+for (let i = 3; i <= n; i++) {
+  dp[i] = dp[i - 1] + dp[i - 2];
+}
+
+return dp[n];
+};
